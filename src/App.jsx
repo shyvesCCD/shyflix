00482f8b0f81cc1142141e711a6b403d45f918cd
Pedro Chaves <shyvesCCD>
2020-07-27 18:49:08 -0300

    :tada: Updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Menu from './components/Menu'
 import dadosIniciais from './data/dados_iniciais.json';
 import Carousel from './components/Carousel';
 import BannerMain from './components/BannerMain';
-// import Footer from './components/Footer'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -40,6 +40,8 @@ function App() {
       <Carousel
         category={dadosIniciais.categorias[5]}
       />
+
+      <Footer/>
 
     </div>
   );
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,13 +4,13 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
+      <a href="https://github.com/shyvesCCD">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
         Orgulhosamente criado durante a
         {' '}
-        <a href="https://www.alura.com.br/">
+        <a href="https://github.com/shyvesCCD">
           Imersão React da Alura
         </a>
       </p>
