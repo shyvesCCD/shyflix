b81843d92d4a0ee8a52f892305d53310f93d9b86
Pedro Chaves <shyvesCCD>
2020-07-28 19:24:17 -0300

    :tada: Commit da Segunda Parte

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import Logo from '../../assents/img/Logo.png'
+import { Link } from 'react-router-dom';
+import Logo from '../../assents/img/Logo.png';
 import './Menu.css';
-import Button from '../Button/index'
+import Button from '../Button/index';
 // import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className ="Logo" src={Logo} alt="Shyvesflix"></img>
-            </a>
+            </Link>
+            
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo video
             </Button>
         </nav>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json';
-import Carousel from './components/Carousel';
-import BannerMain from './components/BannerMain';
-import Footer from './components/Footer'
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json';
+import Carousel from '../../components/Carousel';
+import BannerMain from '../../components/BannerMain';
+import Footer from '../../components/Footer'
 
-function App() {
+function Home() {
   return (
     <div style={{ background: "#141414"}}>
       <Menu></Menu>
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que e Front-end? Trabalhando na area!"}
+        videoDescription={"Palestra sobre codigo limpo, feito por Uncle Bob."}
       />
 
       <Carousel
@@ -47,4 +47,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
